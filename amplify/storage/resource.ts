18c86fc754d667b626da35b19fac0652321395a0
root <root@ip-172-16-64-128.ec2.internal>
2025-12-17 16:40:07 +0000

    updated

---
+++ amplify/storage/resource.ts
@@ -0,0 +1,25 @@
+import { defineStorage } from '@aws-amplify/backend';
+
+export const storage = defineStorage({
+  name: 'neuTestBrowserStorage',
+
+  // 🔹 IMPORTANT: Use existing bucket
+  bucketName: 'neu-test-browser',
+
+  access: (allow) => ({
+    'public/*': [
+      allow.guest.to(['read']),
+      allow.authenticated.to(['read', 'write', 'delete']),
+    ],
+
+    'protected/{entity_id}/*': [
+      allow.authenticated.to(['read']),
+      allow.entity('identity').to(['read', 'write', 'delete']),
+    ],
+
+    'private/{entity_id}/*': [
+      allow.entity('identity').to(['read', 'write', 'delete']),
+    ],
+  }),
+});
+
